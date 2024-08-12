d385c040962d0978008a99ed96f4b6f7cbb5fce8
Miichiko <carolinemikaha@gmail.com>
2024-08-12 16:57:01 -0300

    Finalizado exercício lista de compras

--- basic_python/lista_de _compras.py
+++ basic_python/lista_de _compras.py
@@ -1,22 +1,34 @@
 #  faça uma lista de compras
 
-acao = input("Selecione uma opção...\n[i]nserir, [a]pagar, [l]istar.")
+# 1- faça a lista onde o usuário vai inserir os itens.
+
+
 lista_de_compras = []
 indice = 0
 
-# 1- faça a lista onde o usuário vai inserir os itens.
+while (len(lista_de_compras)) < 10:
 
-while True:
-    if acao == "i":
+    opcao = input("Selecione uma opção...\n[i]nserir, [a]pagar, [l]istar. ")
+
+    if opcao == "i":
         valor = (input("Valor: "))
         lista_de_compras.append(valor)
 
-    for item in enumerate(lista_de_compras):
-        indice, valor = item
-        print(item)
+        if opcao == "a":
 
-        indice += 1
+            if (len(lista_de_compras)) > 0:
+                apagar = input("Escolha o índice para apagar: ")
+                elemento_removido = lista_de_compras.pop(int(apagar))
+            else:
+                print("Erro: Nada para apagar.")
 
-    if acao == "a":
-        valor = (input("Valor: "))
-        lista_de_compras.pop(valor)
+        if opcao == "l":
+            if (len(lista_de_compras)) > 0:
+                for item in enumerate(lista_de_compras):
+                    indice, valor = item
+                    print(item)
+                    indice += 1
+            else:
+                print("Erro: nada para listar")
+    else:
+        print("Por favor, digite: i, a ou l.")
