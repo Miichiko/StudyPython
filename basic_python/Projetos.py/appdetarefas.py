e46e7bd1efc67a8e67e0c9cfb7cecba9b362760d
Miichiko <carolinemikaha@gmail.com>
2024-09-04 07:48:31 -0300

    fiz a função editar e salvar

--- basic_python/Projetos.py/appdetarefas.py
+++ basic_python/Projetos.py/appdetarefas.py
@@ -1,7 +1,7 @@
 import tkinter as tk
 from tkinter import ttk, font, messagebox
 from tkinter import PhotoImage
-from funcoesdoapp import *
+
 
 #  criando a janela
 window = tk.Tk()
@@ -10,34 +10,73 @@ window.configure(bg='#F0F0F0')
 window.geometry('500x600')
 
 tarefas_adicionadas = []
+# corrigir isso depois
+icon_edit = tk.PhotoImage(
+    file='C:/Users/MK10/Desktop/python/basic_python/Projetos.py/edit.png').subsample(4, 4)
+icon_delete = PhotoImage(
+    file='C:/Users/MK10/Desktop/python/basic_python/Projetos.py/delete.png').subsample(4, 4)
 
 
 def botao_adicionar_tarefa():
+    global label_tarefa
     tarefa = entry.get()
     if tarefa in tarefa != '':
         tarefas_adicionadas.append(tarefa)
         # criando uma label no canvas interior
         label_tarefa = tk.Label(canvas_interior, text=tarefa, font=(
-            'Garamond', 16), bg='red', fg='#333', width=25, height=2, anchor='w')
-        label_tarefa.pack(side=tk.LEFT, fill=tk.X, padx=10, pady=5)
+            'Garamond', 16), bg='red', fg='#333', width=40, height=40, anchor='w')
+        label_tarefa.pack(side=tk.LEFT, fill=tk.X, padx=30, pady=30)
         entry.delete(0, tk.END)
     else:
         messagebox.showwarning(
             'Entrada inválida', 'Por favor, insira sua tarefa!')
-    edit_buttom = tk.Button(frame_homework_list, image=icon_edit,
-                            command=lambda f=frame_homework_list, l=label_tarefa: editar_tarefa(), bg='white', relief=tk.FLAT)
-    edit_buttom.pack(side=tk.RIGHT, padx=5)
+    edit_buttom = tk.Button(label_tarefa, image=icon_edit,
+                            command=lambda f=frame_homework_list, l=label_tarefa: editar_tarefa(tarefas_adicionadas), bg='white', relief=tk.FLAT)
+    edit_buttom.pack(side=tk.RIGHT, padx=5, pady=5)
     delete_buttom = tk.Button(frame_homework_list, image=icon_delete,
                               command=lambda f=frame_homework_list: deletar_tarefa(), bg='white', relief=tk.FLAT)
     delete_buttom.pack(side=tk.RIGHT, padx=5)
     frame_homework_list.pack(fill=tk.X, padx=5, pady=5)
 
 
-# corrigir isso depois
-icon_edit = PhotoImage('edit.png'.subsample(3, 3))
-icon_delete = PhotoImage('detele.png'.subsample(3, 3))
+def editar_tarefa(tarefas_adicionadas):
+    global label_tarefa
+    # remove o label atual
+    label_tarefa.pack_forget()
 
-# criando o Título do app
+    # cria um entry pra editar a tarefa
+    entry_edit = tk.Entry(canvas_interior, bg='white')
+    entry_edit.insert(0, "")
+    entry_edit.pack(side=tk.LEFT, fill=tk.X, expand=True)
+
+    # troca o botão editar pelo de salvar
+    salvar_button = tk.Button(
+        canvas_interior, text="Salvar", command=lambda: salvar_edicao(entry_edit))
+    salvar_button.pack(side=tk.RIGHT)
+
+    def salvar_edicao(entry_edit):
+        global tarefas_adicionadas
+
+        # Obtém o novo texto do Entry
+        nova_tarefa = entry_edit.get()
+
+        # Atualiza a lista de tarefas
+        tarefas_adicionadas = nova_tarefa
+
+        # Atualiza o Label com o novo texto
+        label_tarefa.config(text=nova_tarefa)
+
+        # Remove o Entry e exibe o Label novamente
+        entry_edit.pack_forget()
+        salvar_button.pack_forget()
+        label_tarefa.pack(side=tk.LEFT, fill=tk.X, expand=True)
+
+
+def deletar_tarefa():
+    print("michikola")
+
+
+    # criando o Título do app
 font_header = font.Font(family='Garamond', size=24, weight='bold')
 label_header = tk.Label(window, text='Meu App de Tarefas',
                         font=font_header, bg='#F0F0F0', fg='#333').pack(pady=20)
--- basic_python/Projetos.py/funcoesdoapp.py
+++ basic_python/Projetos.py/funcoesdoapp.py
@@ -1,11 +1,39 @@
 # fazer uma função para cada ação do to do (adicionar_tarefa, atualizar_tarefa, concluir_tarefa, deletar_tarefa)
-# Função pra adicionar tarefa
-def adicionar_tarefa():
-    print("michikola")
+import tkinter as tk
+from appdetarefas import label_tarefa, canvas_interior, tarefas_adicionadas
 
 
-def editar_tarefa():
-    print("michikola")
+def editar_tarefa(tarefas_adicionadas):
+    print("entrei aqui")
+    # global label_tarefa
+    # # remove o label atual
+    # label_tarefa.pack_forget()
+
+    # # cria um entry pra editar a tarefa
+    # entry_edit = tk.Entry(canvas_interior, bg='white')
+    # entry_edit.insert(0)
+    # entry_edit.pack(side=tk.LEFT, fill=tk.X, expand=True)
+
+    # # troca o botão editar pelo de salvar
+    # salvar_button = tk.Button(
+    #     canvas_interior, text="Salvar", command=lambda: salvar_edicao(entry_edit))
+    # salvar_button.pack(side=tk.RIGHT)
+
+
+def salvar_edicao(entry_edit):
+    global tarefas_adicionadas
+    # Obtém o novo texto do Entry
+    nova_tarefa = entry_edit.get()
+
+    # Atualiza a lista de tarefas
+    tarefas_adicionadas = nova_tarefa
+
+    # Atualiza o Label com o novo texto
+    label_tarefa.config(text=nova_tarefa)
+
+    # Remove o Entry e exibe o Label novamente
+    entry_edit.pack_forget()
+    label_tarefa.pack(side=tk.LEFT, fill=tk.X, expand=True)
 
 
 def concluir_tarefa():
